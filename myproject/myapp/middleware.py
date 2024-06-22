0002edc390d2ba50349313edcdcd950f51b01b55
LovkiiSanya <Gordienkosan95@gmail.com>
2024-06-22 19:52:51 +0300

    Initial commit

--- myproject/myapp/middleware.py
+++ myproject/myapp/middleware.py
@@ -1,9 +1,6 @@
-# myapp/middleware.py
 import requests
 from jose import jwt, jwk
-from jose.exceptions import JWTError, ExpiredSignatureError
-from django.http import JsonResponse, HttpResponseRedirect
-from django.urls import reverse
+from django.http import JsonResponse
 
 
 class KeycloakTokenMiddleware:
@@ -14,25 +11,29 @@ class KeycloakTokenMiddleware:
         jwks_url = 'http://0.0.0.0:8080/realms/my_blog/protocol/openid-connect/certs'
 
         try:
+            # Получаем JWKS с Keycloak
             jwks_response = requests.get(jwks_url)
             jwks_response.raise_for_status()
             jwks = jwks_response.json()
 
+            # Получаем JWT из заголовка Authorization
             authorization_header = request.META.get('HTTP_AUTHORIZATION', '')
-            print(f"Authorization header value: {authorization_header}")
 
             if not authorization_header.startswith('Bearer '):
                 raise ValueError("Invalid Authorization header. Expected format 'Bearer <token>'.")
 
             jwt_token = authorization_header.split('Bearer ')[1].strip()
-            print(f"Extracted token: {jwt_token}")
 
+            # Извлекаем kid из заголовков JWT
             headers = jwt.get_unverified_headers(jwt_token)
             kid = headers.get('kid')
+
             if not kid:
                 raise ValueError("kid not found in token headers.")
 
             public_key = None
+
+            # Находим открытый ключ по kid в JWKS
             for key in jwks['keys']:
                 if key['kid'] == kid:
                     public_key = jwk.construct(key)
@@ -41,16 +42,20 @@ class KeycloakTokenMiddleware:
             if not public_key:
                 raise ValueError("Public key not found in JWKS")
 
+            # Проверяем и декодируем JWT
             payload = jwt.decode(jwt_token, public_key, algorithms=['RS256'])
             request.jwt_payload = payload
 
         except requests.exceptions.RequestException as e:
+            # Обработка ошибок сети или запроса к JWKS
             print(f"Error fetching JWKS: {e}")
             return JsonResponse({'error': 'Error fetching JWKS'}, status=500)
 
-        except (KeyError, ValueError, JWTError, ExpiredSignatureError) as e:
+        except (KeyError, ValueError) as e:
+            # Обработка ошибок декодирования JWT или работы с ключами
             print(f"JWT decoding or key handling error: {e}")
             return JsonResponse({'error': str(e)}, status=401)
 
+        # Продолжаем выполнение запроса
         response = self.get_response(request)
         return response
